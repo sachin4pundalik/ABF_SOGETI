f7377fbc900759654fe366c55a4e5e066e55970f
AKHAN8 <AKHAN8@LBNL12416.corp.capgemini.com>
2016-06-09 16:29:16 +0100

    Tabs added for AM, KT and Fixed hours

--- ABFUsers/src/main/webapp/angular-components/contract-header/contract-header.directive.js
+++ ABFUsers/src/main/webapp/angular-components/contract-header/contract-header.directive.js
@@ -6,6 +6,24 @@ angular.module('webappApp').directive('contractHeader', function () {
         	isCollapsed:'@collapse'
         },
         templateUrl: './angular-components/contract-header/contract-header.html',
+        controller: ['$scope','$location', function($scope, $location) {
+        	$scope.tabs = [
+        	               { title:'AM Hours' },
+        	               { title:'KT Hours'},
+        	               { title:'Fixed Hours' }
+        	             ];
+        	
+        	$scope.selectTab = function(index){
+        		if(index=='0'){
+        			$location.path('/hours');
+        		}else if(index=='1'){
+        			$location.path('/kt');
+        		}else if(index=='2'){
+        			$location.path('/fixed');
+        		}
+        	}
+        
+        }],
         link: function ($scope, element, attrs) { 
         	
         } //DOM manipulation
